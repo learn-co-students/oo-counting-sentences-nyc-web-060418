8c074c9e1d6295365c8af96cfd64325a92f3e43e
Ms-Alex <ms.hernandeza1@gmail.com>
2018-06-06 17:51:38 -0400

    finished counting sentences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self_arr = self.split
+    count = 0
+    self_arr.each do |str|
+      if str.sentence?
+        count += 1
+      elsif str.question?
+        count += 1
+      elsif str.exclamation?
+        count += 1
+      end
+    end
+    return count
   end
+
 end
+
+
+# str = String.new("How. Are! You?")
+#
+# str.count_sentences
