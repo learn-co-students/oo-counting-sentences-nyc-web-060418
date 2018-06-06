4d276f681adbcc46695c45d3a7b4f423737450f8
noah-berman <noahberm@gmail.com>
2018-06-06 18:14:37 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,29 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence? #return true if the string being called upon ends in a period, and false if not
+    self.end_with?(".") ? true : false
   end
 
-  def question?
-
+  def question? #return true if the string ends in a question mark, return false if not
+    self.end_with?("?") ? true : false
   end
 
-  def exclamation?
-
+  def exclamation? #same but with exclamation mark
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
 
+    new_array = self.split.select do |word|
+      word.sentence? || word.exclamation? || word.question?
+    end
+    new_array.length
+
   end
+
 end
